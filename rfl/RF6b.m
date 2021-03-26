ec437fd6cdbdc245cdab7edc4ce0ec09bced5a7d
kaustubhv5500 <54748807+kaustubhv5500@users.noreply.github.com>
2021-03-26 11:47:57 +0530

    added dtsp paper

--- rfl/RF6b.m
+++ rfl/RF6b.m
@@ -20,6 +20,8 @@ s21 = x + 1i*y;
 [x,y] = pol2cart(-17*pi/180,0.93);
 s22 = x + 1i*y;
 
+s = [s11 s12; s21 s22];
+
 % Defining the given Impedences
 Zs = 50;
 Zl = 50;
@@ -36,8 +38,10 @@ gamma_out = s22 + ((s12*s21*gamma_s)/(1-s11*gamma_s));
 G = (abs(s21)^2)*(1-abs(gamma_l)^2)/((1-abs(gamma_in)^2)*(abs(1-s22*gamma_l)^2));
 G_A = (abs(s21)^2)*(1-abs(gamma_s)^2)/((1-abs(gamma_out)^2)*(abs(1-s11*gamma_s)^2));
 G_T = (abs(s21)^2)*(1-abs(gamma_s)^2)*(1-abs(gamma_l)^2)/((abs(1-gamma_s*gamma_l)^2)*(abs(1-s22*gamma_l)^2));
+G_TU = (abs(s21)^2)*(1-abs(gamma_s)^2)*(1-abs(gamma_l)^2)/((abs(1-s11*gamma_s)^2)*(abs(1-s22*gamma_l)^2));
 
 % Displaying the output parameters
 fprintf('The Gain of the antenna is : %f \n',G);
 fprintf('The Available Power Gain of the antenna is : %f \n',G_A);
 fprintf('The Transducer Power Gain of the antenna is : %f \n',G_T);
+fprintf('The unilateral Transducer Power Gain of the antenna is : %f \n',G_TU);
