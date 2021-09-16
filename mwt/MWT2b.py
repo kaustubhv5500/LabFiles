74634ab51dda5334a4b7f34dd7d1046bfb52a1e8
kaustubhv5500 <54748807+kaustubhv5500@users.noreply.github.com>
2021-09-16 23:13:28 +0530

    Added channel adjustment to MWT2b

--- mwt/MWT2b.py
+++ mwt/MWT2b.py
@@ -8,7 +8,7 @@ from random import sample
 import math
 import numpy as np
 
-total_channels = input("Enter the total number of channels: ")
+total_channels = int(input("Enter the total number of channels: "))
 cluster_size = int(input("Enter the cluster size: "))
 channel_req = list()
 valid_clusters = [1,3,4,7,9,12,13,19,21,27,28,31,37,39,43,48,49,52]
@@ -31,6 +31,27 @@ for i in range(int(cluster_size)):
     s = "Enter requirement for cell " + str(i+1) + ": "
     channel_req.append(int(input(s)))
 
+print('\nOriginal Channel Requirement', channel_req)
+
+if sum(channel_req) > total_channels * 0.15:
+    print('Channel requirement more than total number of available channels!!\nChanging channel allocation based on Blocking Probability.')
+    channels_per_cell = math.ceil(total_channels / cluster_size)
+    max_req = max(channel_req)
+    max_diff = (abs(max_req - channels_per_cell) / (cluster_size - 1)) + channels_per_cell
+    blocking_prob = max_diff / max_req
+    
+    for i in range(len(channel_req)):
+        if channel_req[i] > channels_per_cell:
+            channel_req[i] = math.ceil(blocking_prob * channel_req[i])
+            if sum(channel_req) < total_channels and channel_req[i] !=0:
+                channel_req[i] += math.ceil(abs(total_channels - sum(channel_req)))
+    
+    if sum(channel_req) > total_channels:
+        channel_req[channel_req.index(max(channel_req))] -= abs(total_channels-channel_req)
+    elif sum(channel_req) < total_channels:
+         channel_req[channel_req.index(min(channel_req))] += abs(total_channels-channel_req)
+
+    print('Updated Channel Requirement: ',channel_req)
 total_channels = int(total_channels)
 all_channels = list()
 
@@ -52,9 +73,13 @@ i = 0
 print("\nThe cell assignments are: ")
 for i in range(int(cluster_size)):
     for j in range(int(cluster_size)):
-        num = math.ceil(channel_req[i]/2)
+        num = math.ceil(channel_req[i] / 2)
+        if num > len(highp_channel):
+            num = len(highp_channel) // 2
         cellh = sample(highp_channel,num)
-        celll = sample(lowp_channels, channel_req[i] - num)
+        if (channel_req[i] - num) > len(lowp_channels):
+            num = len(lowp_channels) // 2
+        celll = sample(lowp_channels, num)
         cell = cellh + celll
     highp_channel = list(set(highp_channel).difference(cellh))
     lowp_channels = list(set(lowp_channels).difference(celll))
